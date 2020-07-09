f54a2b6585b9e2619851a1136ece26b076adb912
Courtney Yargo <courtneyyargo@gmail.com>
2020-07-09 00:24:24 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,7 @@
 def speak_to_grandma(phrase)
   if phrase == phrase.downcase
     return "HUH?! SPEAK UP, SONNY!"
-  elseif phrase.upcase
+  elseif == phrase.upcase
     return "NO, NOT SINCE 1938!"
   else phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE TOO PUMPKIN!"
